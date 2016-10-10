215165d4377b689f465cc9922ecd4ed08ee595ef
Marek Bruchatý <marekbruchaty@gmail.com>
2016-10-10 13:31:50 +0200

    Almofire request json from url

--- BeaCodeApp/NetworkServiceManager.swift
+++ BeaCodeApp/NetworkServiceManager.swift
@@ -7,3 +7,27 @@
 //
 
 //ALAMOFIRE
+import Alamofire
+
+public class NetworkServiceManager {
+    
+    static let sharedInstance = NetworkServiceManager()
+    typealias JSONStandard = [String: AnyObject]
+    
+    func request(url: String) {
+        Alamofire.request(url).responseJSON(completionHandler: {
+        response in
+            self.parseData(JSONData: response.data!)
+        })
+    }
+    
+    func parseData(JSONData: Data) {
+        do {
+            let JSONDataReadable = try JSONSerialization.jsonObject(with: JSONData, options: .mutableContainers) as? JSONStandard
+            print(JSONDataReadable)
+        } catch {
+            print(error)
+        }
+    }
+
+}
