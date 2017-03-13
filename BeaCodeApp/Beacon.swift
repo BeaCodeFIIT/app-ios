c52c779e6a0f38258a2a475f5441190e8dbdbbdb
Marek Bruchatý <marekbruchaty@gmail.com>
2017-03-13 02:39:29 +0100

    Repairs on old domain classes

--- BeaCodeApp/Beacon.swift
+++ BeaCodeApp/Beacon.swift
@@ -7,3 +7,21 @@
 //
 
 import Foundation
+
+class Beacon {
+    var id: String?
+    var UUID: String?
+    var major: String?
+    var minor: String?
+    var coorX: String?
+    var coorY: String?
+    
+    init(id: String, UUID: String, major: String, minor: String, coorX: String, coorY: String) {
+        self.id = id
+        self.UUID = UUID
+        self.major = major
+        self.minor = minor
+        self.coorX = coorX
+        self.coorY = coorY
+    }
+}
--- BeaCodeApp/Event.swift
+++ BeaCodeApp/Event.swift
@@ -10,59 +10,12 @@ import Foundation
 import UIKit
 
 struct Event {
-    var title: String!
-    var description: String!
-    var thumbnail: UIImage!
-    var date: Date!
-    var organizer: String!
-    var location: String!
-    var selectedExhibit: Exhibit!
+    var id: String?
+    var name: String?
+    var start: Date?
+    var end: Date?
+    var description: String?
+    var location: Location?
+    var categories: Array<Category>?
     
-    var photos = [UIImage]()
-    var exhibits = [Exhibit]()
-    var categorizedExhibits = CategorizedExhibits()
-    var sections = Array<Section>()
-    
-    
-    func getExpandedSectionsCount() -> Int {
-        var expanded = 0
-        for section in sections {
-            if !section.collapsed {
-                expanded += 1
-            }
-        }
-        return expanded
-    }
-    
-    init() {
-        self.title = ""
-        self.description = ""
-        self.thumbnail = UIImage()
-        self.date = Date()
-        self.organizer = ""
-        self.location = ""
-    }
-    
-    init(title: String, description: String, date: Date, thumbnail: UIImage, organizer: String, location: String) {
-        self.title = title
-        self.description = description
-        self.thumbnail = thumbnail
-        self.date = date
-        self.organizer = organizer
-        self.location = location
-    }
-    
-    init(title: String, description: String, date: Date, thumbnail: UIImage, organizer: String, location: String, photos: [UIImage]) {
-        self.title = title
-        self.description = description
-        self.thumbnail = thumbnail
-        self.date = date
-        self.organizer = organizer
-        self.location = location
-        self.photos = photos
-    }
-    
-    func downloadImage(imageUrl: String) -> UIImage {
-        return UIImage()
-    }
 }
